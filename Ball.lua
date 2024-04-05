fa61ec867c65f611e736e3d7dc2939844395c79d
low-perry <120011256+low-perry@users.noreply.github.com>
2024-04-05 13:54:07 +0200

    Added the main files

---
+++ Ball.lua
@@ -0,0 +1,56 @@
+--[[
+    Ball Class
+    This class represents the ball in the game.
+
+]]
+
+Ball = Class{}
+
+function Ball:init(x, y, width, height)
+    self.x = x
+    self.y = y
+    self.width = width
+    self.height = height
+
+    -- These variables are for keeping track of our velocity on both the
+    -- X and Y axis, since the ball can move in two dimensions.
+    self.dy = 0
+    self.dx = 0
+end
+
+--[[
+    Expects a paddle as an argument and returns true or false, depending
+    on whether their rectangles overlap.
+]]
+function Ball:collides(paddle)
+    -- first, check to see if the left edge of either is farther to the right
+    -- than the right edge of the other
+    if self.x > paddle.x + paddle.width or paddle.x > self.x + self.width then
+        return false
+    end
+
+    -- then check to see if the bottom edge of either is higher than the top
+    -- edge of the other
+    if self.y > paddle.y + paddle.height or paddle.y > self.y + self.height then
+        return false
+    end 
+
+    -- if the above aren't true, they're overlapping
+    return true
+end
+
+function Ball:reset()
+    self.x = VIRTUAL_WIDTH / 2 - 2
+    self.y = VIRTUAL_HEIGHT / 2 - 2
+    self.dx = 0
+    self.dy = 0
+end
+
+function Ball:update(dt)
+    self.x = self.x + self.dx * dt
+    self.y = self.y + self.dy * dt
+end
+
+function Ball:render()
+    love.graphics.rectangle('fill', self.x, self.y, self.width, self.height)
+end
---
+++ Paddle.lua
@@ -0,0 +1,36 @@
+--[[
+    Represent the Paddle object, that interacts with the ball in the game.
+    It moves up and down and deflects the ball back toward the opponent.
+]]
+
+Paddle = Class{}
+
+
+function Paddle:init(x, y, width, height)
+    self.x = x
+    self.y = y
+    self.width = width
+    self.height = height
+    self.dy = 0
+end
+
+function Paddle:update(dt)
+    -- math.max here ensures that we're the greater of 0 or the player's
+    -- current calculated Y position when pressing up so that we don't
+    -- go into the negatives; the movement calculation is simply our
+    -- previously-defined paddle speed scaled by dt
+    if self.dy < 0 then
+        self.y = math.max(0, self.y + self.dy * dt)
+    -- similar to before, this time we use math.min to ensure we don't
+    -- go any farther than the bottom of the screen minus the paddle's
+    -- height (or else it will go partially below, since position is
+    -- based on its top left corner)
+    else
+        self.y = math.min(VIRTUAL_HEIGHT - self.height, self.y + self.dy * dt)
+    end
+end
+
+
+function Paddle:render()
+    love.graphics.rectangle('fill', self.x, self.y, self.width, self.height)
+end
